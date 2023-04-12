b8b476f2e1e653924c1939e89777fc51d09f0827
MichaelSun <zhensherlock@126.com>
2023-04-12 11:19:26 +0800

    feat: update baidu net disk

--- src/devtools/components/BaiduNetDisk.vue
+++ src/devtools/components/BaiduNetDisk.vue
@@ -2,21 +2,10 @@
   <div>
     <el-row :gutter="10">
       <el-col :span="12">
-        <el-input
-          v-model="input"
-          :rows="3"
-          type="textarea"
-          placeholder="请输入百度分享网址"
-        />
+        <el-input v-model="input" :rows="3" type="textarea" placeholder="请输入百度分享网址" />
       </el-col>
       <el-col :span="12">
-        <el-input
-          v-model="output"
-          :rows="3"
-          type="textarea"
-          placeholder=""
-          readonly
-        />
+        <el-input v-model="output" :rows="3" type="textarea" placeholder="" readonly />
       </el-col>
     </el-row>
     <el-row class="m-t-10px">
@@ -25,6 +14,21 @@
         <el-button plain @click="handleReset">重置</el-button>
       </el-col>
     </el-row>
+    <el-collapse class="m-t-10px">
+      <el-collapse-item title="高级配置">
+        <el-form :model="advancedForm" label-width="100px">
+          <el-form-item label="回调地址">
+            <el-input v-model="advancedForm.callback_url" />
+          </el-form-item>
+          <el-form-item label="回调URL参数">
+            <el-input v-model="advancedForm.callback_url_param_key" />
+          </el-form-item>
+          <el-form-item label="重复次数">
+            <el-input-number v-model="advancedForm.repeat_count" />
+          </el-form-item>
+        </el-form>
+      </el-collapse-item>
+    </el-collapse>
   </div>
 </template>
 
@@ -35,15 +39,26 @@ export default {
 </script>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import { runScript, sleep } from '@/utils'
 import { ElMessage } from 'element-plus'
 
-const input = ref('')
+const input = ref('http://127.0.0.1:5000/redirect_link')
 let output = ref('')
 const executeLoading = ref(false)
+const advancedForm = reactive({
+  repeat_count: 1,
+  callback_url: 'http://127.0.0.1:5000/set_transfer_link',
+  callback_url_param_key: 'url'
+})
 
 const handleExecute = async () => {
+  for (const item of new Array(advancedForm.repeat_count).fill(0)) {
+    await handleTransfer()
+  }
+}
+
+const handleTransfer = async () => {
   if (!input.value) {
     ElMessage.error('请输入百度分享网址！')
     return
@@ -71,13 +86,14 @@ const handleExecute = async () => {
     file_list = result
   })
   runScript(`
-const path_node = document.querySelector('.fx-info-section-path-blue');
-const path_node_data = path_node.getAttribute('target-path');
+const path_node = document.querySelector('.fx-info-section-path-blue')
+const path_node_data = path_node ? path_node.getAttribute('target-path') : '/MAC/app'
 location.href = 'https://pan.baidu.com/disk/main?from=homeSave#/index?category=all&path='+ path_node_data
 `)
   await sleep(3000)
   file_list.forEach(item => {
-    runScript(`document.querySelector('[title="${item.server_filename}"]').click()`)
+    runScript(`document.querySelector('[title="${item.server_filename}"]').parentElement.parentElement.
+    parentElement.parentElement.previousSibling.click()`)
   })
   runScript(`document.querySelector('[title="分享"]').click()`)
   await sleep(3000)
@@ -86,9 +102,21 @@ location.href = 'https://pan.baidu.com/disk/main?from=homeSave#/index?category=a
   runScript(`document.querySelector('.wp-s-share-to-link__create-form-submit--button').click()`)
   await sleep(3000)
   runScript(`document.querySelector('.wp-s-share-to-link__link-info-url-wrapper input').value`, (result) => {
-    output.value += result
+    output.value = result
+    executeLoading.value = false
+    handleResultCallback(result)
   })
-  executeLoading.value = false
+  await sleep(3000)
+}
+
+const handleResultCallback = async (url) => {
+  if (!advancedForm.callback_url || !advancedForm.callback_url_param_key) {
+    return
+  }
+  await fetch(`${advancedForm.callback_url}?${advancedForm.callback_url_param_key}=${url}`, {
+    method: 'GET',
+    mode: 'cors'
+  })
 }
 
 const handleReset = () => {
